62e80656f35a14040f94e05974bef10107bbe254
Leshya Bracaglia <leshyabracaglia@gmail.com>
2019-02-16 16:29:35 -0500

    Initial Commit

---
+++ BracagliaAssg1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BracagliaAssg1
+//
+//  Created by Leshya Bracaglia on 2/16/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
